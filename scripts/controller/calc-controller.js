3ac3436993955f3b74af42da36b43ec551290e6b
Danilo <dasf_cm@terra.com.br>
2019-05-02 04:35:08 -0300

    starting calc-controller.js

---
+++ scripts/controller/calc-controller.js
@@ -0,0 +1,33 @@
+class CalcController {
+  constructor() {
+    this._displayCalc = "0"
+    this._currentDate
+    this.initialize()
+  }
+
+  initialize() {
+    let displayCalcEl = document.querySelector("#display")
+    let dateEl = document.querySelector("#data")
+    let timeEl = document.querySelector("#hora")
+
+    displayCalcEl.innerHTML = "4567"
+    dateEl.innerHTML = "02/05/2019"
+    timeEl.innerHTML = "00:00"
+  }
+
+  get displayCalc() {
+    return this._displayCalc
+  }
+
+  set displayCalc(valor) {
+    this._displayCalc = valor
+  }
+
+  get currentDate() {
+    return this._currentDate
+  }
+
+  set currentDate(valor) {
+    this._currentDate = valor
+  }
+}
