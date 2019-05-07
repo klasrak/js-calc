0596491664a547e8f2dcf4ffe5d57db88ecba68c
Danilo <dasf_cm@terra.com.br>
2019-05-07 10:44:58 -0300

    implementing the sound method with double click on the 'ac' button

--- scripts/controller/calc-controller.js
+++ scripts/controller/calc-controller.js
@@ -1,5 +1,7 @@
 class CalcController {
   constructor() {
+    this._audio = new Audio('click.mp3')
+    this._audioOnOff = false
     this._lastOperator = ''
     this._lastNumber = ''
     this._operation = []
@@ -36,10 +38,27 @@ class CalcController {
     }, 1000)
     this.setLastNumberToDisplay()
     this.pasteFromClipboard()
+    document.querySelectorAll('.btn-ac').forEach(btn => {
+      btn.addEventListener('dblclick', e => {
+        this.toggleAudio()
+      })
+    })
+  }
+
+  toggleAudio() {
+    this._audioOnOff = !this._audioOnOff
+  }
+
+  playAudio() {
+    if (this._audioOnOff) {
+      this._audio.currentTime = 0
+      this._audio.play()
+    }
   }
 
   initKeyboard() {
     document.addEventListener('keyup', e => {
+      this.playAudio()
       switch (e.key) {
         case 'Escape': //All Clear
           this.clearAll()
@@ -209,6 +228,7 @@ class CalcController {
   }
 
   execBtn(value) {
+    this.playAudio()
     switch (value) {
       case 'ac': //All Clear
         this.clearAll()
